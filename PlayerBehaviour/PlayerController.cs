e299bfe9bdc333f41a7a61d90c9ba1143ea9162e
RGBSTUDIO_NOTE <rgbstudio69@gmail.com>
2017-06-20 13:56:58 +0300

    v1.01-beginningToWork
    Добавил папку PlayerBehaviour. В ней, с соотвутствующий пространством имен, будут содержаться классы, описывающие поведение персонажа (атака, движение, повотор, перекаты и грабежи караванов)

---
+++ PlayerBehaviour/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PlayerBehaviour
+{
+    /// <summary>
+    /// Используется для управления перонажем.
+    /// Задает движение и поворот.
+    /// </summary>
+    public class PlayerController
+        : MonoBehaviour
+    {
+
+    }
+}
